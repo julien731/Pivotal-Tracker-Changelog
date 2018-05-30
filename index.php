0488d44f41a375e7df8ff0738a080900d11d80a6
Julien Liabeuf <julien@liabeuf.fr>
2018-05-30 19:49:26 +0700

    Load the Pivotal_Changelog class in all cases

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 $project_id      = filter_input( INPUT_GET, 'project_id', FILTER_SANITIZE_NUMBER_INT );
 $release_version = filter_input( INPUT_GET, 'version', FILTER_SANITIZE_STRING );
 
+require( 'includes/Pivotal_Changelog.php' );
+
 if ( empty( $project_id ) || empty( $release_version ) ) :
 	include( 'project.php' );
 else :
-	require( 'includes/Pivotal_Changelog.php' );
 	$changelog = new Pivotal_Changelog( $project_id, $release_version );
 	include( 'changelog.php' );
 endif;
