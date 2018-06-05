038b55927acd53a87006c37dbf0ca1411a2be58d
Julien Liabeuf <julien@liabeuf.fr>
2018-06-05 14:26:40 +0700

    Save user token in a cookie if set and retrieve it

--- web/index.php
+++ web/index.php
@@ -1,11 +1,22 @@
 <?php
 $project_id      = filter_input( INPUT_GET, 'project_id', FILTER_SANITIZE_NUMBER_INT );
 $release_version = filter_input( INPUT_GET, 'version', FILTER_SANITIZE_STRING );
+$user_token      = filter_input( INPUT_GET, 'user_token', FILTER_SANITIZE_STRING );
+
+if ( ! empty( $user_token ) ) {
+	setcookie( 'tracker_user_token', $user_token, time() + 3600 * 24 * 365 );
+}
 
 require( 'includes/Pivotal_Changelog.php' );
 
+$token = Pivotal_Changelog::get_token();
+
 if ( empty( $project_id ) || empty( $release_version ) ) :
-	include( 'project.php' );
+	if ( empty( $token ) ) {
+		include( 'get-token.php' );
+	} else {
+		include( 'project.php' );
+	}
 else :
 	$changelog = new Pivotal_Changelog( $project_id, $release_version );
 	include( 'changelog.php' );
